d4445c2e757c2f4ebb29fb35090bd0d729e3531f
Erik Pilgrim <erikp@sketchbin.com>
2019-10-23 12:44:32 -0500

    Added CustomComponentWith3D component

---
+++ renderdraw-examples/force-app/main/default/aura/CustomComponentWith3D/CustomComponentWith3DController.js
@@ -0,0 +1,11 @@
+({
+    handleRimsClick: function (component, event, helper) {
+        component.set('v.selectedOverrideURL','https://files.renderdraw.us/renderings/public/obj/Rims.obj');
+    },
+    handleFlightClick: function (component, event, helper) {
+        component.set('v.selectedOverrideURL','https://files.renderdraw.us/renderings/public/obj/Flight_24230EU_OBJ.obj');
+    },
+    handlePencilClick: function (component, event, helper) {
+        component.set('v.selectedOverrideURL','https://files.renderdraw.us/renderings/public/obj/RenderDrawPencil.obj');
+    },
+})
